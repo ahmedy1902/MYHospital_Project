e9e2891e1560a7291c4e3029c51c204e850a33c4
ahmedy1902 <ahmedy1902@gmail.com>
2025-05-16 02:27:01 +0300

    after changing home

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -1,36 +1,48 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CareNet_System.Models
-{ 
-    public enum StaffTitle {
-        Doctor, Nurse , Administrative
-}
+{
+    public enum StaffTitle
+    {
+        Doctor, Nurse, Administrative
+    }
+
     public enum Level
     {
-        Senior , Junior
+        Senior, Junior
     }
 
     public class Staff
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name must not exceed 50 characters.")]
         public string name { get; set; }
+
+        [Required(ErrorMessage = "Staff title is required.")]
         [Column(TypeName = "nvarchar(50)")]
         public StaffTitle title { get; set; }
+
+        [Required(ErrorMessage = "Salary is required.")]
+        [Range(5000, 200000, ErrorMessage = "Salary must be between 5000 and 200,000.")]
         public decimal? salary { get; set; }
 
+        [Required(ErrorMessage = "Seniority level is required.")]
         [Column(TypeName = "nvarchar(50)")]
         public Level seniority_level { get; set; }
-        public int experience_years { get; set; }
-        public string? personal_photo { get; set; }
-        [ForeignKey("department")]
 
+        [Required(ErrorMessage = "Experience years are required.")]
+        [Range(0, 50, ErrorMessage = "Experience years must be between 0 and 50.")]
+        public int experience_years { get; set; }
+
+        public string? personal_photo { get; set; }
+
+        [ForeignKey("department")]
         public int dept_id { get; set; }
 
         public Department? department { get; set; }
-
         public Patient? patients { get; set; }
-        
-
-
     }
 }
