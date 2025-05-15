141a2cd23bc9c657e8f1f3df12d72fdfd9197284
ahmedy1902 <ahmedy1902@gmail.com>
2025-05-16 01:25:55 +0300

    Staff Controler

---
+++ Controllers/StaffsController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CareNet_System.Models;
+
+namespace CareNet_System.Controllers
+{
+    public class StaffsController : Controller
+    {
+        private readonly HosPitalContext _context;
+
+        public StaffsController(HosPitalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Staffs
+        public async Task<IActionResult> Index()
+        {
+            var hosPitalContext = _context.Staff.Include(s => s.department);
+            return View(await hosPitalContext.ToListAsync());
+        }
+
+        // GET: Staffs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staff
+                .Include(s => s.department)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return View(staff);
+        }
+
+        // GET: Staffs/Create
+        public IActionResult Create()
+        {
+            ViewData["dept_id"] = new SelectList(_context.Departments, "Id", "Id");
+
+            ViewBag.TitleList = Enum.GetValues(typeof(StaffTitle))
+                .Cast<StaffTitle>()
+                .Select(t => new SelectListItem { Value = t.ToString(), Text = t.ToString() })
+                .ToList();
+
+            ViewBag.LevelList = Enum.GetValues(typeof(Level))
+                .Cast<Level>()
+                .Select(l => new SelectListItem { Value = l.ToString(), Text = l.ToString() })
+                .ToList();
+
+            return View();
+        }
+
+        // POST: Staffs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,name,title,salary,seniority_level,experience_years,personal_photo,dept_id")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(staff);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["dept_id"] = new SelectList(_context.Departments, "Id", "Id", staff.dept_id);
+            return View(staff);
+        }
+
+        // GET: Staffs/Edit/5
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) { return NotFound(); }
+
+            var staff = await _context.Staff.FindAsync(id);
+            if (staff == null) { return NotFound(); }
+
+            ViewData["dept_id"] = new SelectList(_context.Departments, "Id", "Id", staff.dept_id);
+
+            ViewBag.TitleList = Enum.GetValues(typeof(StaffTitle))
+                .Cast<StaffTitle>()
+                .Select(t => new SelectListItem { Value = t.ToString(), Text = t.ToString() })
+                .ToList();
+
+            ViewBag.LevelList = Enum.GetValues(typeof(Level))
+                .Cast<Level>()
+                .Select(l => new SelectListItem { Value = l.ToString(), Text = l.ToString() })
+                .ToList();
+
+            return View(staff);
+        }
+
+        // POST: Staffs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,name,title,salary,seniority_level,experience_years,personal_photo,dept_id")] Staff staff)
+        {
+            if (id != staff.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(staff);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StaffExists(staff.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["dept_id"] = new SelectList(_context.Departments, "Id", "Id", staff.dept_id);
+            return View(staff);
+        }
+
+        // GET: Staffs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staff
+                .Include(s => s.department)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return View(staff);
+        }
+
+        // POST: Staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var staff = await _context.Staff.FindAsync(id);
+            if (staff != null)
+            {
+                _context.Staff.Remove(staff);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StaffExists(int id)
+        {
+            return _context.Staff.Any(e => e.Id == id);
+        }
+    }
+}
