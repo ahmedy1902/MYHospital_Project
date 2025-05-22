135137b94effb8af13ee696e363be747f2cf751e
ahmedy1902 <ahmedy1902@gmail.com>
2025-05-22 21:34:30 +0300

    after fixing depts

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -2,25 +2,39 @@
 using Microsoft.AspNetCore.Authorization;
 using CareNet_System.Models;
 using CareNet_System.Repository;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CareNet_System.Controllers
 {
     [Authorize]
     public class DepartmentsController : Controller
     {
+        private readonly IRepository<Staff> _staffRepository;
+
         private readonly IDepartmentRepository _deptRepo;
 
-        public DepartmentsController(IDepartmentRepository deptRepo)
+        public DepartmentsController(IDepartmentRepository deptRepo , IRepository<Staff> staffRepository)
         {
+            _staffRepository = staffRepository;
+
             _deptRepo = deptRepo;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string managerFilter)
         {
-            var deptList = _deptRepo.GetAll();
-            return View(deptList);
-        }
+            var departments = _deptRepo.GetAll();
 
+            ViewBag.ManagerList = new SelectList(
+                departments.Select(d => d.manager).Distinct().ToList()
+            );
+
+            if (!string.IsNullOrEmpty(managerFilter))
+            {
+                departments = departments.Where(d => d.manager == managerFilter).ToList();
+            }
+
+            return View(departments);
+        }
         public IActionResult Details(int id)
         {
             var department = _deptRepo.GetById(id);
@@ -30,6 +44,12 @@ namespace CareNet_System.Controllers
 
         public IActionResult Create()
         {
+            ViewBag.ManagerList = new SelectList(
+                _staffRepository.GetAll().Where(s => s.title == StaffTitle.Doctor), 
+                "Id",
+                "name"
+            );
+
             return View();
         }
 
